27c496f73268b9b53414a14e32ba71bfce0187f1
THANG Nguyen Huu (VTI.D1) <thang.nguyenhuu@vti.com.vn>
2022-12-14 18:42:03 +0700

    add

--- js/index.js
+++ js/index.js
@@ -14,11 +14,25 @@ $(".close").click(function () {
     500
   );
 });
-
+const btn = document.getElementById('avatar-index');
+btn.onclick= function(){
+  const div = document.getElementById('avatar-dropdown');
+  div.classList.toggle('display-flex')
+}
 $('.slider-index-tab').slick({
   infinite: true,
   slidesToShow: 5,
   slidesToScroll: 1,
-  prevArrow: false,
-  nextArrow: false
+  prevArrow: $('.left'),
+  nextArrow:$('.right'),
 });
+
+// if (window.matchMedia('screen and (max-width: 1324px)').matches){
+//   $('.row-new-cource').slick({
+//     infinite: true,
+//     slidesToShow: 4,
+//     slidesToScroll: 1,
+//     prevArrow: false,
+//     nextArrow: false
+//   });
+// }
